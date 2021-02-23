c8df8b20cc9517ae1fc0070134799ce847dc8fd0
skylardarkfox <skylardarkfox@gmail.com>
2021-02-23 16:41:43 -0600

    Imported and reformatted PreparedStatement

--- php/Database/PreparedStatement.php
+++ php/Database/PreparedStatement.php
@@ -1,8 +1,47 @@
 <?php
+declare(strict_types=1);
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace KitsuneTech\Velox;
 
+class PreparedStatement extends Query {
+    private array $_namedParams = [];
+    private int $_paramCount = 0;
+    private array $_paramArray = [];
+    private ?string $setId = null;
+    
+    public function __construct(Connection $conn, string $sql, ?string $keyColumn = null, int $queryType = QUERY_SELECT, int $resultType = VELOX_RESULT_UNION, ?string $setId = null) {
+        parent::__construct($conn,$sql,$keyColumn,$queryType,$resultType);
+        if (preg_match_all("/:[A-Za-z0-9]+/",$sql,$this->_namedParams) > 0){
+            $this->_paramCount = count($this->_namedParams);
+        }
+        else {
+            $this->_paramCount = preg_match_all("/\?/",$sql);
+        }
+        $this->setId = $setId;
+    }
+    public function addParameterSet(array $paramArray) : void {
+        foreach ($paramArray as $key => $value){
+            $paramArray[":".$key] = $value;
+            unset($paramArray[$key]);
+        }
+        $this->_paramArray[] = $paramArray;
+    }
+    public function getParams() : array {
+        return $this->_paramArray;
+    }
+    public function getParamCount() : int {
+        return $this->_paramCount;
+    }
+    public function clear() : void {
+        $this->_paramArray = [];
+    }
+    public function dumpQuery(): array {
+        $dump = parent::dumpQuery();
+        $dump["type"] = "PreparedStatement";
+        $dump["parameters"] = $this->_paramArray;
+        return $dump;
+    }
+    public function getSetId() : ?string {
+        return $this->setId;
+    }
+}
