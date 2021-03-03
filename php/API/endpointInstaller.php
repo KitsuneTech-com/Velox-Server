fa11180dc93ef0d37171f9e6f3f4012b8b8f033b
skylardarkfox <skylardarkfox@gmail.com>
2021-03-02 19:02:59 -0600

    Update endpointInstaller.php
    TODO: set relative path of composer autoloader for each endpoint (likely string manipulation at require_once line)

--- php/API/endpointInstaller.php
+++ php/API/endpointInstaller.php
@@ -1,5 +1,18 @@
 #!/usr/bin/env php
 <?php
+function relativePath($from, $to, $ps = DIRECTORY_SEPARATOR){
+  $arFrom = explode($ps, rtrim($from, $ps));
+  $arTo = explode($ps, rtrim($to, $ps));
+  while(count($arFrom) && count($arTo) && ($arFrom[0] == $arTo[0]))
+  {
+    array_shift($arFrom);
+    array_shift($arTo);
+  }
+  return str_pad("", count($arFrom) * 3, '..'.$ps).implode($ps, $arTo);
+}
+
+//Command flow
+
 echo "Velox API endpoint post-installer";
 echo "---------------------------------";
 echo "Would you like to configure Velox API endpoints? (y/[n])\n";
