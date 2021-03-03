e2727ecd2a42b784d98a02a2fdb959e6543edd94
skylardarkfox <skylardarkfox@gmail.com>
2021-03-03 16:30:47 -0600

    Update endpointInstaller.php

--- php/API/endpointInstaller.php
+++ php/API/endpointInstaller.php
@@ -4,8 +4,8 @@ function relativePath($from, $to, $ps = DIRECTORY_SEPARATOR){
 	$arFrom = explode($ps, rtrim($from, $ps));
 	$arTo = explode($ps, rtrim($to, $ps));
 	while(count($arFrom) && count($arTo) && ($arFrom[0] == $arTo[0])){
-    	array_shift($arFrom);
-    	array_shift($arTo);
+        array_shift($arFrom);
+        array_shift($arTo);
   	}
 	return str_pad("", count($arFrom) * 3, '..'.$ps).implode($ps, $arTo);
 }
@@ -43,7 +43,7 @@ if ($answer == "y"){
 	   		} 
 		}
 		else {
-	    	break;
+            break;
 		}
     }
     echo "Endpoint creation finished.\n";
