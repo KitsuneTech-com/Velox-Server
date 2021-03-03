76f203e31961084abb32e70f6e6f36894f6ec6c5
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2021-03-03 17:13:32 -0600

    Update endpointInstaller.php

--- php/API/endpointInstaller.php
+++ php/API/endpointInstaller.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
-function relativePath($from, $to, $ps = DIRECTORY_SEPARATOR){
+//Necessary utility functions
+function relativePath($from, $to, $ps = DIRECTORY_SEPARATOR) : string {
     $arFrom = explode($ps, rtrim($from, $ps));
     $arTo = explode($ps, rtrim($to, $ps));
     while(count($arFrom) && count($arTo) && ($arFrom[0] == $arTo[0])){
@@ -10,10 +11,35 @@ function relativePath($from, $to, $ps = DIRECTORY_SEPARATOR){
     return str_pad("", count($arFrom) * 3, '..'.$ps).implode($ps, $arTo);
 }
 
+function copy_dir($src,$dst) : bool {
+    $dir = opendir($src);
+    if (!is_dir($dst)){
+        if (!mkdir($dst)){
+            return false;
+        }
+    }
+    while( ( $file = readdir($dir)) !== false ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                if (!copy_dir($src . '/' . $file,$dst . '/' . $file)){
+                    return false;
+                }
+            }
+            else {
+                if (!copy($src . '/' . $file,$dst . '/' . $file)){
+                    return false;
+                }
+            }
+        }
+    }
+    closedir($dir);
+    return true;
+}
+
+// ---- Execution begins here ---- //
+
 //get current file path
 $thisDir = dirname(__FILE__);
-
-//Command flow
 
 echo "Velox API endpoint post-installer\n";
 echo "---------------------------------\n";
@@ -29,12 +55,12 @@ if ($answer == "y"){
         $apipath = trim(fgets(STDIN));
         if ($apipath != ""){
             $fullpath = rtrim($webpath,"/")."/".$apipath;
-            echo "cp ".$thisDir."/index.php ".$fullpath."/index.php\n";
-            $index = exec("cp ".$thisDir."/index.php ".$fullpath."/index.php");
+            echo "Copying ".$thisDir."/index.php to ".$fullpath."/index.php...\n";
+            $index = copy($thisDir."/index.php",$fullpath."/index.php");
             if ($index){
-                echo "API endpoint created at ".$fullpath.".\n";
-                echo "cp -r ".$thisDir."/queries ".$fullpath."/queries\n";
-                $queries = exec("cp -r ".$thisDir."/queries ".$fullpath."/queries");
+                echo "API endpoint created at ".$fullpath.".\n\n";
+                echo "Copying queries directory...\n";
+                $queries = copy_dir($thisDir."/queries",$fullpath."/queries");
                 if ($queries){
                     echo "Queries subdirectory created at ".$fullpath."/queries\n";
                 }
