503a31139a32ddea3237982366cd95704a70ab53
skylardarkfox <skylardarkfox@gmail.com>
2021-03-04 09:56:26 -0600

    Update index.php

--- php/API/index.php
+++ php/API/index.php
@@ -6,17 +6,15 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { exit(); } //Return CORS preflight headers
 
-//Normal requests start here
+//Path to autoloader substituted by post-install script
+require_once $autoloaderPath;
 
-require_once 'config/config.php';
-require_once 'php/errorReporting.php';
-require_once 'php/core.php';
-
-use KitsuneTech\Velox;
+use KitsuneTech\Velox\VeloxException as VeloxException;
+use KitsuneTech\Velox\Structures\{Model, Diff};
 
 //The endpoint uses the 'q' GET parameter to find the appropriate query definition, so this parameter must be sent on the request.
 if (!isset($_GET['q'])){
-    throw new Velox\VeloxException("No query name specified. Name must be specified as a '?q=' GET parameter on the request.",1);
+    throw new VeloxException("No query name specified. Name must be specified as a '?q=' GET parameter on the request.",1);
 }
 else {
     $queryName = $_GET['q'];
@@ -26,7 +24,7 @@ $queryFileName = $GLOBALS['VeloxQueryPath'].$queryName.".php";
 
 //Make sure the query definition file exists in the defined location.
 if (!file_exists($queryFileName)){
-    throw new Velox\VeloxException('Query definition file does not exist for query "'.$queryName.'"',2);
+    throw new VeloxException('Query definition file does not exist for query "'.$queryName.'"',2);
 }
 
 $SELECT = $_POST['select'] ?? null;
@@ -36,7 +34,7 @@ $DELETE = $_POST['delete'] ?? null;
 $META = $_POST['meta'] ?? null;
 
 if ($SELECT || $UPDATE || $INSERT || $DELETE){
-    $DIFF = new Velox\Diff();
+    $DIFF = new Diff();
     $DIFF->select = json_decode($SELECT);
     $DIFF->update = json_decode($UPDATE);
     $DIFF->insert = json_decode($INSERT);
@@ -54,14 +52,14 @@ require_once $queryFileName;
 ////////-- Query definition code execution --/////////
 
 if (isset($QUERIES['SELECT'])){
-    $VELOX_MODEL = new Velox\Model($QUERIES['SELECT'] ?? null, $QUERIES['UPDATE'] ?? null, $QUERIES['INSERT'] ?? null, $QUERIES['DELETE'] ?? null);
+    $VELOX_MODEL = new Model($QUERIES['SELECT'] ?? null, $QUERIES['UPDATE'] ?? null, $QUERIES['INSERT'] ?? null, $QUERIES['DELETE'] ?? null);
     if ($DIFF){
 	$VELOX_MODEL->synchronize($DIFF);
     }
     //Set the query version header (if it's set as a positive integer)
     if ($QUERY_VERSION ?? false){
 	if (!is_int($QUERY_VERSION)){
-	    throw new Velox\VeloxException('Incorrect version value set in query definition (query "'.$queryName.'"',4);
+	    throw new VeloxException('Incorrect version value set in query definition (query "'.$queryName.'"',4);
 	}
     }
 }
