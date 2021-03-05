74e62bf62615bb9a6442a1675d07a1ca5842af9a
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2021-03-05 09:43:47 -0600

    Create manualPostinstall.php

---
+++ manualPostinstall.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+//Composer seems to have an issue with running interactive post-install scripts, so this is
+//an alternate means of accomplishing the same. Use the php binary to run this file after
+//Composer has installed the library.
+
+echo "Velox web component installation\n";
+echo "--------------------------------\n";
+echo "Enter the full absolute path of your website root, or hit Enter to use the directory\n";
+$webpath = trim(fgets(STDIN));
+
+echo "Would you like to configure API endpoint(s)? [y/n] (n)\n";
+$answer = trim(fgets(STDIN));
+if ($answer == "y") {
+    include_once('php/API/endpointInstaller.php');
+}
