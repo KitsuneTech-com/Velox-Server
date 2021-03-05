9d1a08c6bea7bd10e888f6317e62daf134b23080
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2021-03-05 09:17:50 -0600

    Update endpointInstaller.php

--- php/API/endpointInstaller.php
+++ php/API/endpointInstaller.php
@@ -44,63 +44,59 @@ function copy_dir($src,$dst) : bool {
 $sep = DIRECTORY_SEPARATOR;
 
 // ---- Execution begins here ---- //
-
+echo "\n";
 echo "Velox API endpoint installation\n";
 echo "-------------------------------\n";
-echo "Would you like to configure Velox API endpoints? [y/n] (n)\n";
-$answer = trim(fgets(STDIN));
-if ($answer == "y"){
-    echo "Enter the full absolute path of your website root.\n";
-    $webpath = trim(fgets(STDIN));
-    $finished = false;
-    while (true){
-        echo "Enter the site-relative endpoint path for each endpoint you wish ";
-        echo "to create, or hit Enter to finish.\n";
-        $apiPath = trim(fgets(STDIN));
-        if ($apiPath != ""){
-            echo "\n";
-            $fullPath = rtrim($webpath,$sep).$sep.$apiPath;
-            $directory = false;
-            if (!is_dir($fullPath)){
-                echo "Directory ".$fullPath." does not exist. Creating...\n";
-                if (!mkdir($fullPath)){
-                    echo "Error: Could not create directory ".$fullPath.".\n\n";
-                }
-                else {
-                    $directory = true;
-                }
+echo "Enter the full absolute path of your website root.\n";
+$webpath = trim(fgets(STDIN));
+$finished = false;
+while (true){
+    echo "Enter the site-relative endpoint path for each endpoint you wish ";
+    echo "to create, or hit Enter to finish.\n";
+    $apiPath = trim(fgets(STDIN));
+    if ($apiPath != ""){
+        echo "\n";
+        $fullPath = rtrim($webpath,$sep).$sep.$apiPath;
+        $directory = false;
+        if (!is_dir($fullPath)){
+            echo "Directory ".$fullPath." does not exist. Creating...\n";
+            if (!mkdir($fullPath)){
+                echo "Error: Could not create directory ".$fullPath.".\n\n";
             }
-            if ($directory){
-                $endpointPath = $fullPath.$sep."index.php";
-                $queriesPath = $fullPath.$sep."queries";
-                echo "Copying ".__DIR__.$sep."index.php to ".$endpointPath."...\n";
-                $index = copy(__DIR__.$sep."index.php",$endpointPath);
-                if ($index){
-                    echo "API endpoint created at ".$fullPath.".\n";
-                    
-                    echo "Setting relative path to autoloader...\n";
-                    $relPath = relativePath($fullPath,realpath(__DIR__."$sep..$sep..$sep..$sep..$sep")).$sep."autoload.php";
-                    $endpointFile = file_get_contents($endpointPath);
-                    $endpointFile = str_replace('/path/to/autoloader',$relPath,$endpointFile);
-                    file_put_contents($endpointPath,$endpointFile);
-                    
-                    echo "Copying queries directory...\n";
-                    $queries = copy_dir(__DIR__.$sep."queries",$queriesPath);
-                    if ($queries){
-                        echo "Queries subdirectory created at ".$queriesPath."\n\n";
-                    }
-                    else {
-                        echo "Error: Could not create queries subdirectory at ".$queriesPath."\n\n";
-                    }
-                }
-                else {
-                    echo "Error: could not create API endpoint at ".$endpointPath.".\n\n";
-                }
+            else {
+                $directory = true;
             }
         }
-        else {
-            break;
+        if ($directory){
+            $endpointPath = $fullPath.$sep."index.php";
+            $queriesPath = $fullPath.$sep."queries";
+            echo "Copying ".__DIR__.$sep."index.php to ".$endpointPath."...\n";
+            $index = copy(__DIR__.$sep."index.php",$endpointPath);
+            if ($index){
+                echo "API endpoint created at ".$fullPath.".\n";
+                    
+                echo "Setting relative path to autoloader...\n";
+                $relPath = relativePath($fullPath,realpath(__DIR__."$sep..$sep..$sep..$sep..$sep")).$sep."autoload.php";
+                $endpointFile = file_get_contents($endpointPath);
+                $endpointFile = str_replace('/path/to/autoloader',$relPath,$endpointFile);
+                file_put_contents($endpointPath,$endpointFile);
+                    
+                echo "Copying queries directory...\n";
+                $queries = copy_dir(__DIR__.$sep."queries",$queriesPath);
+                if ($queries){
+                    echo "Queries subdirectory created at ".$queriesPath."\n\n";
+                }
+                else {
+                    echo "Error: Could not create queries subdirectory at ".$queriesPath."\n\n";
+                }
+            }
+            else {
+                echo "Error: could not create API endpoint at ".$endpointPath.".\n\n";
+            }
         }
     }
-    echo "Endpoint creation finished.\n";
+    else {
+        break;
+    }
 }
+echo "Endpoint creation finished.\n";
