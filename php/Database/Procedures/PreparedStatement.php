661bce5954457707828103264c84736b066ab180
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2021-03-16 18:19:39 -0500

    Added by-ref operator on constructor

--- php/Database/Procedures/PreparedStatement.php
+++ php/Database/Procedures/PreparedStatement.php
@@ -11,7 +11,7 @@ class PreparedStatement extends Query {
     private array $_paramArray = [];
     private ?string $setId = null;
     
-    public function __construct(Connection $conn, string $sql, ?string $keyColumn = null, int $queryType = QUERY_SELECT, int $resultType = VELOX_RESULT_UNION, ?string $setId = null) {
+    public function __construct(Connection &$conn, string $sql, ?string $keyColumn = null, int $queryType = QUERY_SELECT, int $resultType = VELOX_RESULT_UNION, ?string $setId = null) {
         parent::__construct($conn,$sql,$keyColumn,$queryType,$resultType);
         if (preg_match_all("/:[A-Za-z0-9]+/",$sql,$this->_namedParams) > 0){
             $this->_paramCount = count($this->_namedParams);
