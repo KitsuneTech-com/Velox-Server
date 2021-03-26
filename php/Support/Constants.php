83ad67458c6dcf7a41ee29ebd7ca804c39a36cb9
skylardarkfox <skylardarkfox@gmail.com>
2021-03-26 15:32:59 -0500

    Add MBSTRING_SUPPORT constant
    If the mbstring extension is loaded, this is set to true. This constant saves the potential overhead of calling extension_loaded('mbstring') potentially thousands of times (once for every value in a filtered Model) when the result will not change during the execution of a script.

--- php/Support/Constants.php
+++ php/Support/Constants.php
@@ -37,3 +37,6 @@ if(!defined('STDIN'))	{ define('STDIN',  fopen('php://stdin',  'rb')); }
 if(!defined('STDOUT'))	{ define('STDOUT', fopen('php://stdout', 'wb')); }
 if(!defined('STDERR'))	{ define('STDERR', fopen('php://stderr', 'wb')); }
 if(!isset($GLOBALS['Velox'])) { $GLOBALS['Velox'] = []; }
+
+//Determine mbstring support at runtime
+define('MBSTRING_SUPPORT', extension_loaded('mbstring'));
