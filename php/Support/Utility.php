d8464cb9c0c4c56f02dc7889de87c97104a9aaa4
skylardarkfox <skylardarkfox@gmail.com>
2021-03-27 12:53:46 -0500

    sqllike_comp() WIP

--- php/Support/Utility.php
+++ php/Support/Utility.php
@@ -10,65 +10,42 @@ function recur_ksort(&$array) {
     return ksort($array);
 }
 
-function sqllike_comp(mixed $value1, string $op, mixed $value2, bool $exact = false) : bool {
+function sqllike_comp(mixed $value1, string $op, mixed $value2) : bool {
+    //This is based on and functionally equivalent to MySQL comparison operations.
     $v1_type = gettype($value1);
     $v2_type = gettype($value2);
-    if ($exact){
-        if ($v1_type != $v2_type) return false; //Mismatched types automatically fail an exact match
-        switch ($op){
-            case "=":
+    
+    if ($v1_type == "string") $value1 = MBSTRING_SUPPORT ? mb_strtolower($value) : strtolower($value);
+    switch ($op){
+        case "=":
+            if ($v1_type == "string" && $v2_type == "string"){
+                //Use strict comparison with strings to avoid numeric typecasting (which MySQL does not do when comparing two strings)
                 return $value1 === $value2;
-            case "<":
-                return $value1 < $value2;
-            case ">":
-                return $value1 > $value2;
-            case "<=":
-                return $value1 <= $value2;
-            case ">=":
-                return $value1 >= $value2;
-            case "<>":
-                return $value1 !== $value2;
-            case "LIKE":
-            case "NOT LIKE":
-                //Convert SQL wildcards to PCRE syntax
-                //*Note: this introduces overhead that could slow processing. It's better to use RLIKE and NOT RLIKE with regexp syntax. 
-                $value2 = str_replace("%",".*",$value2);
-                $value2 = str_replace("_",".",$value2);
-                //fall through to RLIKE / NOT RLIKE case
-            case "RLIKE":
-            case "NOT RLIKE":
-                return (bool)preg_match('/^'.$value2.'$/',$value1);
-            default:
-                throw new VeloxException("Unsupported operator",36);
-        }
-    }
-    else {
-        //Rely on PHP type coercion if $exact is false
-        switch ($op){
-            case "=":
+            }
+            else {
                 return $value1 == $value2;
-            case "<":
-                return $value1 < $value2;
-            case ">":
-                return $value1 > $value2;
-            case "<=":
-                return $value1 <= $value2;
-            case ">=":
-                return $value1 >= $value2;
-            case "<>":
-                return $value1 !== $value2;
-            case "LIKE":
-            case "NOT LIKE":
-                //Convert SQL wildcards to PCRE syntax
-                //*Note: this introduces overhead that could slow processing. It's better to use RLIKE and NOT RLIKE with regexp syntax. 
-                $value2 = str_replace("%",".*",$value2);
-                $value2 = str_replace("_",".",$value2);
-                //fall through to RLIKE / NOT RLIKE case
-            case "RLIKE":
-            case "NOT RLIKE":
-                return (bool)preg_match('/^'.$value2.'$/',$value1);
-            default:
-                throw new VeloxException("Unsupported operator",36);
-        }
+            }
+        case "<":
+            return $value1 < $value2;
+        case ">":
+            return $value1 > $value2;
+        case "<=":
+            return $value1 <= $value2;
+        case ">=":
+            return $value1 >= $value2;
+        case "<>":
+            return $exact ? ($value1 !== $value2) : ($value1 != $value2);
+        case "LIKE":
+        case "NOT LIKE":
+            //Convert SQL wildcards to PCRE syntax
+            //*Note: this introduces overhead that could slow processing. It's better to use RLIKE and NOT RLIKE with regexp syntax. 
+            $value2 = str_replace("%",".*",$value2);
+            $value2 = str_replace("_",".",$value2);
+            //fall through to RLIKE / NOT RLIKE case
+        case "RLIKE":
+        case "NOT RLIKE":
+            return (bool)preg_match('/^'.$value2.'$/',$value1);
+        default:
+            throw new VeloxException("Unsupported operator",36);
     }
 }
