5b6fe1c31e0dce21595c785380f964f260553d99
skylardarkfox <skylardarkfox@gmail.com>
2021-03-27 14:34:26 -0500

    sqllike_comp() added
    For use in Model->filter()

--- php/Support/Utility.php
+++ php/Support/Utility.php
@@ -10,12 +10,20 @@ function recur_ksort(&$array) {
     return ksort($array);
 }
 
+function isAscii($str) {
+    return preg_match('/[^\x00-\x7F]/', $str) == 0;
+}
+
 function sqllike_comp(mixed $value1, string $op, mixed $value2) : bool {
     //This is based on and functionally equivalent to MySQL comparison operations.
     $v1_type = gettype($value1);
     $v2_type = gettype($value2);
     
-    if ($v1_type == "string") $value1 = MBSTRING_SUPPORT ? mb_strtolower($value) : strtolower($value);
+    //Convert strings to lowercase (use strtolower() if possible due to performance)
+    //(do this because SQL string comparisons are case-insensitive)
+    if ($v1_type == "string") $value1 = MBSTRING_SUPPORT && !isAscii($value1) ? mb_strtolower($value1) : strtolower($value1);
+    if ($v2_type == "string") $value2 = MBSTRING_SUPPORT && !isAscii($value2) ? mb_strtolower($value2) : strtolower($value2);
+    
     switch ($op){
         case "=":
             if ($v1_type == "string" && $v2_type == "string"){
@@ -34,7 +42,7 @@ function sqllike_comp(mixed $value1, string $op, mixed $value2) : bool {
         case ">=":
             return $value1 >= $value2;
         case "<>":
-            return $exact ? ($value1 !== $value2) : ($value1 != $value2);
+            return $value1 != $value2;
         case "LIKE":
         case "NOT LIKE":
             //Convert SQL wildcards to PCRE syntax
