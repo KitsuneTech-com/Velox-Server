536ff154c9d1ad94dfe85624e0d8530adcbbfff4
skylardarkfox <skylardarkfox@gmail.com>
2021-03-30 13:18:59 -0500

    Update Model.php

--- php/Structures/Model.php
+++ php/Structures/Model.php
@@ -273,7 +273,6 @@ class Model {
         }
     }
     public function setFilter(Diff|array|null $filter) : void {
-        print_r($filter);
         $this->_filter = $filter instanceof Diff ? $filter->select[0]['where'] : (!is_null($filter) ? $filter[0]['where'] : []);
         $this->_filteredIndices = [];
         print_r($this->_filter);
