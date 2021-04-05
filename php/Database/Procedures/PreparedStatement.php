45608236e2b0e863746689d02f99294c527ef8ae
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2021-04-05 12:49:32 -0500

    Removed keyColumn parameter
    Not currently implemented

--- php/Database/Procedures/PreparedStatement.php
+++ php/Database/Procedures/PreparedStatement.php
@@ -11,8 +11,8 @@ class PreparedStatement extends Query {
     private array $_paramArray = [];
     private ?string $setId = null;
     
-    public function __construct(Connection &$conn, string $sql, ?string $keyColumn = null, int $queryType = QUERY_SELECT, int $resultType = VELOX_RESULT_UNION, ?string $setId = null) {
-        parent::__construct($conn,$sql,$keyColumn,$queryType,$resultType);
+    public function __construct(Connection &$conn, string $sql, int $queryType = QUERY_SELECT, int $resultType = VELOX_RESULT_UNION, ?string $setId = null) {
+        parent::__construct($conn,$sql,$queryType,$resultType);
         if (preg_match_all("/:[A-Za-z0-9]+/",$sql,$this->_namedParams) > 0){
             $this->_paramCount = count($this->_namedParams);
         }
