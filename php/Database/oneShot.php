901ca86f0a402ca5760142bbbf9869ebc2d86cc5
skylardarkfox <skylardarkfox@gmail.com>
2021-08-17 18:00:24 -0500

    Adjusted oneShot for optional input
    oneShot() can now be used to call a standalone query without specific input

--- php/Database/oneShot.php
+++ php/Database/oneShot.php
@@ -5,20 +5,26 @@ use KitsuneTech\Velox\VeloxException;
 use KitsuneTech\Velox\Database\Procedures\{PreparedStatement, StatementSet, Transaction};
 use KitsuneTech\Velox\Structures\ResultSet as ResultSet;
 
-function oneShot(PreparedStatement|StatementSet|Transaction $query, array $input) : array|ResultSet|bool|null {
+function oneShot(PreparedStatement|StatementSet|Transaction $query, array $input = null) : array|ResultSet|bool|null {
     if ($query instanceof PreparedStatement){
-        $query->addParameterSet($input);
+        if ($input){
+            $query->addParameterSet($input);
+        }
         $query->execute();
         return $query->getResults();
     }
     elseif ($query instanceof StatementSet){
-        $query->addCriteria($input);
+        if ($input){
+            $query->addCriteria($input);
+        }
         $query->execute();
         return $query->getResults();
     }
     elseif ($query instanceof Transaction){
-        //This will need to be changed to $query->addInput() when the nested queries update is merged
-        $query->addParameterSet($input);
+        if ($input){
+            //This will need to be changed to $query->addInput() when the nested queries update is merged
+            $query->addParameterSet($input);
+        }
         $query->begin();
         $query->executeAll();
         return $query->getQueryResults();
