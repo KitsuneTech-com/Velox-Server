508651e28cd015d3c6cf9e852bb2630893cdaed7
skylardarkfox <79388221+skylardarkfox@users.noreply.github.com>
2022-07-11 18:23:06 -0500

    Added recursive previous

--- php/Support/ErrorHandling.php
+++ php/Support/ErrorHandling.php
@@ -6,27 +6,31 @@ if (!isset($GLOBALS['Velox']['ErrorReportingMode'])){
     $GLOBALS['Velox']['ErrorReportingMode'] = VELOX_ERR_STDERR + VELOX_ERR_STACKTRACE;
 }
 function VeloxExceptionHandler(\Throwable $ex) : void {
+    function getExceptionObject(\Throwable $ex) : object {
+        $exObj = (object)['timestamp'=>time(), 'class'=>get_class($ex), 'code'=>$ex->getCode(), 'file'=>$ex->getFile(), 'line'=>$ex->getLine(), 'message'=>$ex->getMessage()];
+        if ($GLOBALS['Velox']['ErrorReportingMode'] & VELOX_ERR_STACKTRACE){
+	        $exObj->trace = $ex->getTrace();
+        }
+        $exObj->previous = $ex->getPrevious();
+        return $exObj;
+    }
     if ($GLOBALS['Velox']['ErrorReportingMode'] === VELOX_ERR_NONE){
-    	echo $ex->code;
+        echo $ex->code;
     }
-    $exObj = (object)['timestamp'=>time(), 'class'=>get_class($ex), 'code'=>$ex->getCode(), 'file'=>$ex->getFile(), 'line'=>$ex->getLine(), 'message'=>$ex->getMessage()];
-    if ($GLOBALS['Velox']['ErrorReportingMode'] & VELOX_ERR_STACKTRACE){
-	$exObj->trace = $ex->getTrace();
-    }
-    $exObj->previous = $ex->getPrevious();
-    $outputObj = (object)['Exception'=>$exObj];
-    if ($GLOBALS['Velox']['ErrorReportingMode'] & VELOX_ERR_STDERR){
-	fwrite(STDERR,$exObj->class." [".$exObj->code."] encountered in ".$exObj->file." (line ".$exObj->line."): ".$exObj->message);
-	if (isset($exObj->trace)){
-	    $traceStr = 'Stack trace:\n';
-	    foreach ($exObj->trace as $item){
-		$traceStr .= "Function ".$item['function']." in ".$item['file']." , line ".$item['line']."\n";
-	    }
-	}
+    else {
+        $outputObj = (object)['Exception'=>getExceptionObject($ex)];
+        if ($GLOBALS['Velox']['ErrorReportingMode'] & VELOX_ERR_STDERR){
+        fwrite(STDERR,$exObj->class." [".$exObj->code."] encountered in ".$exObj->file." (line ".$exObj->line."): ".$exObj->message);
+        if (isset($exObj->trace)){
+            $traceStr = 'Stack trace:\n';
+            foreach ($exObj->trace as $item){
+                $traceStr .= "Function ".$item['function']." in ".$item['file']." , line ".$item['line']."\n";
+            }
+        }
     }
     if ($GLOBALS['Velox']['ErrorReportingMode'] & VELOX_ERR_JSONOUT){
-	header('Content-Type: application/json');
-	echo json_encode($outputObj, JSON_PRETTY_PRINT);
+        header('Content-Type: application/json');
+        echo json_encode($outputObj, JSON_PRETTY_PRINT);
     }
 }
 
