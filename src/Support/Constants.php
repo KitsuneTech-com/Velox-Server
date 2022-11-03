bbb1a0e1fa383ea4d803b2d33fdf74bf3f2cbbc9
skylardarkfox <skylardarkfox@gmail.com>
2022-11-03 18:12:20 -0500

    Added DocBlocks; shifted relevant constants to Query class

--- src/Support/Constants.php
+++ src/Support/Constants.php
@@ -4,12 +4,6 @@ const VELOX_ERR_STDERR = 1;
 const VELOX_ERR_JSONOUT = 2;
 const VELOX_ERR_STACKTRACE = 4;
 
-const VELOX_RESULT_NONE = 0;
-const VELOX_RESULT_ARRAY = 1;
-const VELOX_RESULT_UNION = 2;
-const VELOX_RESULT_UNION_ALL = 3;
-const VELOX_RESULT_FIELDS = 4;
-
 const TO_BROWSER = 1;
 const TO_FILE = 2;
 const TO_STRING = 4;
@@ -20,11 +14,7 @@ const AS_XML = 32;
 const AS_HTML = 64;
 const AS_CSV = 128;
 
-const QUERY_SELECT = 1;
-const QUERY_UPDATE = 2;
-const QUERY_INSERT = 3;
-const QUERY_DELETE = 4;
-const QUERY_PROC = 5;
+
 
 const VELOX_SUPPORTED_OPERATORS = [
     "=",">","<",">=","<=","<>","BETWEEN","IN","LIKE","NOT BETWEEN","NOT IN","NOT LIKE"
