da22ae99cfd33f247705f68cf67fc0dccb4fb31b
skylardarkfox <skylardarkfox@gmail.com>
2025-01-07 13:09:45 -0600

    StatementSet PHPDoc WIP

--- src/Database/Procedures/StatementSet.php
+++ src/Database/Procedures/StatementSet.php
@@ -78,7 +78,7 @@ class StatementSet implements \Countable, \Iterator, \ArrayAccess {
 
     /** StatementSet constructor
      *
-     *  @param Connection $conn The Connection instance to use for this instance.
+     *  @param Connection &$conn The Connection instance to use for this instance (passed by reference).
      *  @param string $_baseSql The SQL template by which to generate the prepared statements.
      *  @param int|null $queryType The type of query to be run (see the QUERY_ constants in {@see Query}).
      *  @param array|VeloxQL $criteria The initial criteria to be applied; this can be used to avoid having to call {@see StatementSet::setCriteria()} later.
