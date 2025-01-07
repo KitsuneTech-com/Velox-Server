969f00b3361b41f4f28ca60001242b86979618af
skylardarkfox <skylardarkfox@gmail.com>
2025-01-07 13:45:23 -0600

    StatementSet PHPDoc WIP

--- src/Database/Procedures/StatementSet.php
+++ src/Database/Procedures/StatementSet.php
@@ -79,13 +79,13 @@ class StatementSet implements \Countable, \Iterator, \ArrayAccess {
     public ResultSet|array|bool|null $results;
 
     /**
-     *  @param Connection $conn The Connection to use for this StatementSet (passed by reference).
-     *  @param string $baseSql The SQL template by which to generate the prepared statements.
-     *  @param int|null $queryType The type of query to be run (see the QUERY_ constants in {@see Query}).
-     *  @param array|VeloxQL $criteria The initial criteria to be applied; this can be used to avoid having to call {@see StatementSet::setCriteria()} later.
-     *  @param string|null $name The name of this StatementSet; this can be used to distinguish between multiple StatementSets in a single Transaction.
+     * @param Connection $conn          The Connection to use for this StatementSet (passed by reference).
+     * @param string $baseSql           The SQL template by which to generate the prepared statements.
+     * @param int|null $queryType       The type of query to be run (see the QUERY_ constants in {@see Query}).
+     * @param array|VeloxQL $criteria   The initial criteria to be applied; this can be used to avoid having to call {@see StatementSet::setCriteria()} later.
+     * @param string|null $name         The name of this StatementSet; this can be used to distinguish between multiple StatementSets in a single Transaction.
      *
-     *  @throws VeloxException if the provided SQL is a stored procedure call (these are not supported)
+     * @throws VeloxException if the provided SQL is a stored procedure call (these are not supported)
      */
     public function __construct(public Connection &$conn, string $baseSql = "", public ?int $queryType = null, public array|VeloxQL $criteria = [], public ?string $name = null) {
         $this->_baseSql = $baseSql;
